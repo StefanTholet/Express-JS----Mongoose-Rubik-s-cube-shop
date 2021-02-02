7f0fb8397906d1a88fdffadc2f507c1f5e13ec20
StefanTholet <stefan.tholet@gmail.com>
2021-02-02 13:40:55 +0200

    Added search functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,40 +1,45 @@
-const { Router } = require('express');
+const { Router, query } = require('express');
 const router = Router();
 const productService = require('../services/productService');
 const accessoryService = require('../services/accessoryService');
+
 router.get('/', async (req, res) => {
-    const cubes = await productService.getAll() 
-    res.render('home', {title: 'Browse Products', cubes})
+    const cubes = await productService.getAll()
+    res.render('home', { title: 'Browse Products', cubes })
 });
 
 router.get('/about', (req, res) => {
-    
-    res.render('about', {title: 'About us'});
+
+    res.render('about', { title: 'About us' });
 });
 
+router.get('/products-search', async (req, res) => {
+    const cubes = await productService.searchCubes(req.query)
+    res.render('home', {title: 'Browse Products', cubes})
+})
+
 router.get('/products/add-cube', (req, res) => {
-    res.render('create', {title: 'Add a cube'})
+    res.render('create', { title: 'Add a cube' })
 });
 
 router.post('/products/add-cube', async (req, res) => {
     try {
-    const cube = await productService.create(req.body);
-} catch(err) {
-    console.log(err);
-    return;
-}
+        const cube =  productService.create(req.body);
+    } catch (err) {
+        console.log(err);
+        return;
+    }
     res.redirect('/')
 })
 
 router.get('/products/add-accessory', (req, res) => {
-    res.render('createAccessory', {title: 'Add an accessory'})
+    res.render('createAccessory', { title: 'Add an accessory' })
 })
 
-router.post('/products/add-accessory', async(req, res) => {
+router.post('/products/add-accessory', async (req, res) => {
     try {
         const accessory = await accessoryService.create(req.body);
-        console.log(accessory);
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         return;
     }
@@ -44,20 +49,19 @@ router.post('/products/add-accessory', async(req, res) => {
 router.get('/products/:productId/attach', async (req, res) => {
     const cube = await productService.getOne(req.params.productId);
     const accessories = await accessoryService.getUnAttached(cube.accessories);
-    res.render('attachAccessory', {title: 'Attach accessory to cube', cube, accessories})
+    res.render('attachAccessory', { title: 'Attach accessory to cube', cube, accessories })
 });
 
 router.post('/products/:productId/attach', async (req, res) => {
-    const cube = await productService.getOne(req.params.productId);
-    const attachPromise = await productService.attachAccessory(cube, req.body.accessory);
-    res.redirect('/products/:productId/details')
-    console.log(cube)
-})
+    const productId = req.params.productId;
+    const attachPromise = await productService.attachAccessory(productId, req.body.accessory);
+    res.redirect(`/products/${productId}/details`)
+});
 
 router.get('/products/:productId/details', async (req, res) => {
     let cube = await productService.getCubeAndAccessories(req.params.productId);
-    const accessories = cube.accessories;  
-    res.render('details', {title: 'Product details', cube, accessories})
+    const accessories = cube.accessories;
+    res.render('details', { title: 'Product details', cube, accessories })
 });
 
 
--- services/productService.js
+++ services/productService.js
@@ -4,24 +4,39 @@ function create(cubeSpecs) {
     return Cube(cubeSpecs).save()
 }
 
- function getAll() {
+function getAll() {
     return Cube.find({}).lean();
 }
 
- function getOne(id) {
+function getOne(id) {
     return Cube.findById(id).lean();
 }
 
 function getCubeAndAccessories(id) {
     return Cube.findById(id)
-    .populate('accessories')
-    .lean();
+        .populate('accessories')
+        .lean();
 }
 
-function attachAccessory(cube, accessoryId) {
-    cube
-    .accessories.push(accessoryId)
-    .save()        
+async function attachAccessory(id, accessoryId) {
+    const cube = await Cube.findById(id);
+    cube.accessories.push(accessoryId);
+    return cube.save();
+}
+
+async function searchCubes(query) {
+    let result = await getAll();
+    console.log(result)
+    if (query.search) {
+      result = result.filter(x => x.name.toLowerCase().includes(query.search.toLowerCase()));
+    };
+    if (query.from) {
+        result = result.filter(x => x.difficultyLevel >= Number(query.from));
+    }
+    if (query.to) {
+        result = result.filter(x => x.difficultyLevel <= Number(query.to));
+    }
+    return result;
 }
 
 module.exports = {
@@ -29,5 +44,6 @@ module.exports = {
     create,
     getAll,
     attachAccessory,
-    getCubeAndAccessories
+    getCubeAndAccessories,
+    searchCubes
 }
